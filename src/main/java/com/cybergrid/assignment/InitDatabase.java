b2458852cbe37d10083ce15addb37c52be37f7d2
gregor-pirc <gregor.pirc@protonmail.com>
2024-08-27 09:12:37 +0200

    Fixed database data preloading

--- src/main/java/com/cybergrid/assignment/InitDatabase.java
+++ src/main/java/com/cybergrid/assignment/InitDatabase.java
@@ -4,14 +4,21 @@ import com.cybergrid.assignment.model.Product;
 import com.cybergrid.assignment.repository.ProductRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class InitDatabase {
     private static final Logger log = LoggerFactory.getLogger(InitDatabase.class);
 
-    CommandLineRunner initDatabase(ProductRepository repository) {
-        return args -> log.info("Preloading " + repository.save(Product.builder().name("Casio A1000").price(5999).currency("EUR").build()));
+    @Autowired
+    private ProductRepository repository;
+
+    @Bean
+    CommandLineRunner initProductDatabase(ProductRepository repository) {
+        return args -> log.info("Preloading " + repository.save(Product.builder().id(1).name("Casio A1000").price(5999).currency("EUR").build()));
     }
+
 }
